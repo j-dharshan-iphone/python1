efd4313b6a8f26fe01ea21b6072c27c60ad43f72
j-dharshan-iphone <jdharshan636@gmail.com>
2025-07-16 17:21:32 +0530

    try example

---
+++ try_except.py
@@ -0,0 +1,15 @@
+
+try:
+    a=int(input("enter a no"))
+    b=int(input("enter a no"))
+    c = a*b
+    print("multiplied the value: ",c)
+
+    
+except:
+    print("Enter only integers")
+
+
+
+
+
